e9156ed8e80aa47fece1e5fa12e57b17bc8e45b0
Omar <180954465+somerand0mcatt@users.noreply.github.com>
2024-11-12 17:33:59 +0200

    :3

--- js/flavor-selector.js
+++
@@ -1,6 +0,0 @@
-const selector = document.querySelector(".flavor-selector");
-document.documentElement.setAttribute("flavor", selector.value);
-selector.addEventListener("change", function () {
-    localStorage.setItem("flavor", this.value);
-    document.documentElement.setAttribute("flavor", this.value);
-});
